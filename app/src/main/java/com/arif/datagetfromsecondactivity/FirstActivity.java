17d10e13b287fcaa6f88cd69df93af1f0fda13d0
arif emran <arifemran018@gmail.com>
2022-04-22 10:42:23 +0600

    Get data from second activity

---
+++ app/src/main/java/com/arif/datagetfromsecondactivity/FirstActivity.java
@@ -0,0 +1,43 @@
+package com.arif.datagetfromsecondactivity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FirstActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        button = findViewById(R.id.button1Id);
+        textView = findViewById(R.id.textViewId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent,1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==1){
+            String value = data.getStringExtra("key");
+            textView.setText(value);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/arif/datagetfromsecondactivity/SecondActivity.java
@@ -0,0 +1,37 @@
+package com.arif.datagetfromsecondactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SecondActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button = findViewById(R.id.button2Id);
+        editText = findViewById(R.id.editTextId);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String value = editText.getText().toString();
+                Intent intent = new Intent(SecondActivity.this, FirstActivity.class);
+                intent.putExtra("key",value);
+                setResult(1,intent);
+                finish();
+
+            }
+        });
+
+    }
+}
